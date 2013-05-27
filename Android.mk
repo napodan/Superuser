15d5de4612fcda1e0754a0a8e8dad1232d0f8f8d
napodan <david.guenet@gmail.com>
2013-05-27 22:25:55 +0200

    Using prebuilt.mk instead of COPY_FILES

--- Android.mk
+++ Android.mk
@@ -1,3 +1,10 @@
 LOCAL_PATH := $(call my-dir)
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/prebuilt/Superuser.apk:system/app/Superuser.apk
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := Superuser.apk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_APPS)
+LOCAL_SRC_FILES := prebuilt/Superuser.apk
+include $(BUILD_PREBUILT)
 
